2491d813a9ccd485099241796f5d69a26cf3d2ae
Vishakha-03 <103671711+Vishakha-03@users.noreply.github.com>
2022-04-19 15:27:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Assignment from './Component/Assignment';
+
+function App() {
+  return (
+    <div className="App">
+      <Assignment/>
+    </div>
+  );
+}
+
+export default App;
